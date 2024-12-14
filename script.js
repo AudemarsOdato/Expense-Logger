f30159cf8a9a55e2aedda730a916e5c5209c289d
Audemars Odato <audemarsodato@gmail.com>
2024-12-14 17:35:02 +0800

    Improved UI
    - Renamed title
    - Changed input layout
    - Improved user friendly UI

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const price = document.querySelector("[name='price']")
 const historyList = document.getElementById("historyList")
 const form = document.querySelector("form")
 
-function onload() {
+function onload() 
+{
         if (!(localStorage.getItem("recordHistory") === null)) {
                 getRecord()
         }
 }
 onload()
 
-function log (itemLabel, itemPrice) {
+function log (itemLabel, itemPrice) 
+{
         if (item.value && price.value) {
+                
                 let currentTime = getTime()
                 listRecordHistory(currentTime, itemLabel, itemPrice)
 
@@ -21,27 +24,47 @@ function log (itemLabel, itemPrice) {
                 price.value = ""
 
                 saveRecord()
+
+        }
+        else {
+
+                if (!(item.value) && price.value) {
+                        alert("Please enter category.")
+                }
+                else if (!(price.value) && item.value) {
+                        alert("Please enter amount.")
+                }
+                else {
+                        alert("Empty record.")
+                }
+                
         }
 }
 
-function undo() {
-        historyList.removeChild(historyList.firstElementChild)
-        saveRecord()
+function undo() 
+{
+        if (confirm("Delete previous record?")) {
+                historyList.removeChild(historyList.firstElementChild)
+                saveRecord()
+        }
 }
 
-function listRecordHistory(time, item, price) {
+function listRecordHistory(time, item, price) 
+{
         const li = document.createElement("li")
         li.textContent = `${time} - ${item} - Php ${price}`
         historyList.prepend(li)
 }
 
-function addToList(record) {
+function addToList(record) 
+{
         const li = document.createElement("li")
         li.textContent = record
         historyList.prepend(li)
 }
 
-function saveRecord() {
+function saveRecord() 
+{
         const recordData = []
         let list = document.querySelectorAll("li")
         list.forEach(record => {
@@ -50,12 +73,14 @@ function saveRecord() {
         localStorage.setItem("recordHistory", JSON.stringify(recordData))
 }
 
-function getRecord() {
+function getRecord() 
+{
         const recordHistory = JSON.parse(localStorage.getItem("recordHistory"))
         recordHistory.forEach(addToList)
 }
 
-function getTime() {
+function getTime() 
+{
         const date = new Date()
         let hour = date.getHours()
         let minute = date.getMinutes()
@@ -72,7 +97,8 @@ function getTime() {
         return `${hour}:${minute} am`
 }
 
-form.onsubmit = (event) => {
+form.onsubmit = (event) => 
+{
         event.preventDefault()
         log(item.value, price.value)
 }
